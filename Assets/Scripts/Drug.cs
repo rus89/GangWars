57ee23e59ec78b4f04179ef7d7106164314a4cca
Milan Rusimov <rusmil89@gmail.com>
2022-01-31 16:34:28 +0100

    !X - Drug Script added
    Why:
    
    Must have for the game

---
+++ Assets/Scripts/Drug.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace LotusGangWars
+{
+    public class Drug : MonoBehaviour
+    {
+        public enum TradingTypeEnum
+        {
+            Inventory,
+            Market
+        }
+        
+        public enum DrugTypeEnum
+        {
+            Acid,
+            Heroin,
+            PCP,
+            Speed,
+            Cocaine,
+            Ludes,
+            Peyote,
+            Weed,
+            Hashish,
+            Opium,
+            Shrooms,
+            MDA
+        }
+
+        public TradingTypeEnum TradingType;
+        public DrugTypeEnum DrugType;
+        public float DrugPrice;
+        public int DrugAmount;
+        public CitiesEnum[] AvailableCities;
+    }
+}
